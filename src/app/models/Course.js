d7b58e1a6fdb821905e3d70b4c70a47d3980b711
Nhan Dat <nnd2004@gmail.com>
2025-01-11 18:52:10 +0700

    update logic sort and ui

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,28 +1,40 @@
-const mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator');
-const mongooseDelete = require('mongoose-delete');
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
+const mongooseDelete = require("mongoose-delete");
 
 const Schema = mongoose.Schema;
 
-const Course = new Schema(
-    {
-        name: { type: String, required: true },
-        description: { type: String },
-        image: { type: String },
-        videoId: { type: String, required: true },
-        level: { type: String },
-        slug: { type: String, slug: 'name', unique: true },
-    },
-    {
-        timestamps: true,
-    },
+const CourseSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true },
+    level: { type: String },
+    slug: { type: String, slug: "name", unique: true },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+// custom query helpers
+
+CourseSchema.query.sortStable = function (req) {
+  if (req.query.hasOwnProperty("_sort")) {
+    const isValidtype = ["asc", "desc"].includes(req.query.type);
+    return this.sort({
+      [req.query.column]: isValidtype ? req.query.type : "desc",
+    });
+  }
+  return this;
+};
 
 // Add plugins
 mongoose.plugin(slug);
-Course.plugin(mongooseDelete, {
-    deletedAt: true,
-    overrideMethods: 'all',
+CourseSchema.plugin(mongooseDelete, {
+  deletedAt: true,
+  overrideMethods: "all",
 });
 
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model("Course", CourseSchema);
